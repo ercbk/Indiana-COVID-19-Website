082adb69af0bb248fe27e3a445932d90d077f0ad
ercbk <ericbookax0@yahoo.com>
2020-07-29 11:02:27 -0400

    new feature text added to tweet

--- tweet/tweet-noon-update.R
+++ tweet/tweet-noon-update.R
@@ -63,7 +63,8 @@ pngs <- png_files %>%
 # need "@<account>" to post on a reply thread
 msg <- glue::glue("@StateHealthIN More charts and analysis at
                   Static Charts: https://bit.ly/2Cdq33q
-                  COVIDcast Dashboard: https://bit.ly/2VSOM44")
+                  COVIDcast Dashboard: https://bit.ly/2VSOM44
+                  ICYMI I've added a table for cases per 100K and positivity rates to the dashboard. Details on About page.")
 
 msg_e <- glue::glue("Indiana COVID-19 Tracker noon update. More charts and analysis
                   Static Charts: https://bit.ly/2Cdq33q
