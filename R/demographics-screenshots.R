e90748174d495f664960095b1ea923b893e2d1ab
ercbk <ericbookax0@yahoo.com>
2020-08-14 09:29:06 -0400

    demo-png - changed file name format to match other tracker pngs

--- R/demographics-screenshots.R
+++ R/demographics-screenshots.R
@@ -2,22 +2,24 @@
 
 library(glue)
 
+today <- lubridate::today()
+
 # age group, tests, deaths bubble chart
 webshot::webshot(glue("{rprojroot::find_rstudio_root_file()}/images/demographics/age-bubble.html"),
-                 file = glue("{rprojroot::find_rstudio_root_file()}/images/demographics/age-bubble.png"),
+                 file = glue("{rprojroot::find_rstudio_root_file()}/images/demographics/med-age-bubble-{today}.png"),
                  delay = 5)
 
 # age group cases per 1000 heatmap
 webshot::webshot(glue("{rprojroot::find_rstudio_root_file()}/images/demographics/heat-cap.html"),
-                 file = glue("{rprojroot::find_rstudio_root_file()}/images/demographics/heat-cap.png"),
+                 file = glue("{rprojroot::find_rstudio_root_file()}/images/demographics/cases-heat-cap-{today}.png"),
                  delay = 5)
 
 # age group cases heatmap
 webshot::webshot(glue("{rprojroot::find_rstudio_root_file()}/images/demographics/heat-cases.html"),
-                 file = glue("{rprojroot::find_rstudio_root_file()}/images/demographics/heat-cases.png"),
+                 file = glue("{rprojroot::find_rstudio_root_file()}/images/demographics/cases-heat-cases-{today}.png"),
                  delay = 5)
 
 # age group, deaths, facetted line chart
 webshot::webshot(glue("{rprojroot::find_rstudio_root_file()}/images/demographics/age-deaths.html"),
-                 file = glue("{rprojroot::find_rstudio_root_file()}/images/demographics/age-deaths.png"),
+                 file = glue("{rprojroot::find_rstudio_root_file()}/images/demographics/age-deaths-line-{today}.png"),
                  delay = 5)
